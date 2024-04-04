6f2e53b6a24ef85988bc756946d4e4e9fd4dcfd0
NathanHu2024 <159183463+NathanHu2024@users.noreply.github.com>
2024-04-04 14:04:32 -0400

    Update 1.2-Variables.js

--- Unit01/1.2-Variables.js
+++ Unit01/1.2-Variables.js
@@ -2,7 +2,7 @@ let length = 10;
 let width = 5;
 area = length * width;
 console.log(area);
-console.log("Area:", area)
+console.log("Area:", area);
 
 let first_name = "Santa";
 let last_name = "Claws";
